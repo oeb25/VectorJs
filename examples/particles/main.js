9007849a6a0827de0796e2e312e20feccd7b77af
Oliver Bøving <lvrmlbvng@gmail.com>
2015-06-01 01:01:13 +0200

    Added uglify to build process. Also added an example

---
+++ examples/particles/main.js
@@ -0,0 +1,87 @@
+import V from '../';
+
+const canvas = document.createElement('canvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = 1280;
+canvas.height = 720;
+
+document.body.appendChild(canvas);
+
+let particles = [];
+
+const trails = false;
+
+function addParticle(amt = 1) {
+  particles.push({
+    pos: V(canvas.width / 2 - 2.5, canvas.height / 2),
+    vel: V.random(-10, 10),
+    speed: Math.random() * 2 + 1
+  });
+
+  if (amt > 1)
+    addParticle(amt - 1);
+}
+
+let last = 0;
+
+const draw = (a) => {
+  let delta = a - last;
+  last = a;
+
+  let fps = 16 / delta;
+
+  requestAnimationFrame(draw);
+
+  addParticle((delta / 16) * 10);
+
+  particles = particles.map(particle => {
+    var { pos, vel, speed } = particle;
+
+    var outX = (pos.x + vel.x > canvas.width || pos.x + vel.x < 0) ? -1 : 1;
+    var outY = pos.y + vel.y > canvas.height ? -1 : 1;
+
+    if (outX != 1 || outY != 1) {
+      var decay = V.random(0.5, .99);
+
+      decay.x = outX == -1 ? decay.x * -1 : 1;
+      decay.y = outY == -1 ? decay.y * -1 : 1;
+
+      vel = vel.mul(decay);
+    }
+
+    vel = vel.add(V(0, 0.1));
+
+    pos = vel.mul(speed).add(pos);
+
+    return {
+      pos, vel, speed
+    };
+  });
+
+  if (trails) {
+    ctx.fillStyle = 'rgba(10,10,10,0.2)';
+  } else {
+    ctx.fillStyle = 'rgb(10,10,10)';
+  }
+
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  var color = ctx.fillStyle = `hsl(${((fps) * 120).toString(10)},100%,50%)`;
+
+  particles.forEach(particle => {
+    ctx.beginPath();
+    ctx.fillRect(particle.pos.x, particle.pos.y, 5, 5);
+  });
+
+  ctx.fillStyle = '#1e1e1e';
+
+  ctx.fillRect(5, 2, 80, 30)
+
+  ctx.fillStyle = color;
+
+  ctx.fillText('FPS: ' + Math.floor(fps * 60), 10, 15);
+  ctx.fillText('Particles: ' + particles.length, 10, 25);
+};
+
+draw();
--- test/index.js
+++ test/index.js
@@ -1,113 +1,109 @@
-var assert = require('assert');
+import assert from 'assert';
+import Vector from '../';
 
-var Vector = require('../');
-
-describe('Vector', function() {
-  describe('Construction', function() {
+describe('Vector', () => {
+  describe('Construction', () => {
     var a = new Vector();
     var b = Vector(2);
 
-    it('should return a vector when new is present', function() {
+    it('should return a vector when new is present', () => {
       assert.equal(true, a instanceof Vector);
     });
 
-    it('should return a vector when new is not present', function() {
+    it('should return a vector when new is not present', () => {
       assert.equal(true, b instanceof Vector);
     });
 
-    describe('default paramertes', function() {
-      it('should default to 0 on both x and y', function() {
+    describe('default paramertes', () => {
+      it('should default to 0 on both x and y', () => {
         assert.equal(0, a.x);
         assert.equal(0, a.y);
       });
 
-      it('should use x as y, if y isn not present', function() {
+      it('should use x as y, if y isn not present', () => {
         assert.equal(b.x, b.y);
       });
 
-      it('shouldn not default to 0 if x is present', function() {
+      it('should not default to 0 if x is present', () => {
         assert.notEqual(0, b.y);
       });
     });
   });
 
-  describe('Helpers', function() {
+  describe('Helpers', () => {
     var a = Vector(5, 10);
     var b = Vector(6, 2);
 
-    var aString = a.x + ',' + a.y;
-    var bString = b.x + ',' + b.y;
-
-    describe('#toString()', function() {
-      it('should return ' + aString, function() {
-        assert.equal(aString, a.toString());
+    describe('#toString()', () => {
+      it(`should return '${a.x},${a.y}'`, () => {
+        assert.equal(`${a.x},${a.y}`, a.toString());
       });
-      it('should return ' + bString, function() {
-        assert.equal(bString, b.toString());
+      it(`should return '${b.x},${b.y}'`, () => {
+        assert.equal(`${b.x},${b.y}`, b.toString());
       });
     });
 
-    describe('#clone()', function() {
+    describe('#clone()', () => {
       var copy = a.clone();
 
-      it('should return an exact copy of the vector', function() {
+      it('should return an exact copy of the vector', () => {
         assert.equal(a.x, copy.x);
         assert.equal(a.y, copy.y);
       });
 
-      it('shouldn not return the object it self', function() {
+      it('should not return the object it self', () => {
         assert.notEqual(a, copy);
       });
     });
 
-    describe('#equals()', function() {
+    describe('#equals()', () => {
       var aClone = Vector(a.x, a.y);
 
-      it('should return true when the vectors are the same', function() {
+      it('should return true when the vectors are the same', () => {
         assert.equal(true, a.equals(aClone));
       });
 
-      it('shouldn not return true when the vectors are diffrent', function() {
+      it('should not return true when the vectors are diffrent', () => {
         assert.equal(false, a.equals(b));
       });
     });
   });
 
-  describe('Math', function() {
+  describe('Math', () => {
     var a = Vector(6, 1);
     var b = Vector(10, 4);
 
-    describe('#add()', function() {
+    describe('#add()', () => {
       var result = a.add(b);
 
-      it('should return the sum of the two vectors ', function() {
+      it('should return the sum of the two vectors ', () => {
         assert.equal(a.x + b.x, result.x);
         assert.equal(a.y + b.y, result.y);
       });
     });
 
-    describe('#sub()', function() {
+    describe('#sub()', () => {
       var result = a.sub(b);
 
-      it('should return the first vector minus the last vector', function() {
+      it('should return the first vector minus the last vector', () => {
         assert.equal(a.x - b.x, result.x);
         assert.equal(a.y - b.y, result.y);
       });
     });
 
-    describe('#mul()', function() {
+    describe('#mul()', () => {
       var result = a.mul(b);
 
-      it('should return the product of the two vectors', function() {
+      it('should return the product of the two vectors', () => {
         assert.equal(a.x * b.x, result.x);
         assert.equal(a.y * b.y, result.y);
       });
     });
 
-    describe('#div()', function() {
+    describe('#div()', () => {
       var result = a.div(b);
 
-      it('should return the first vector divded by the last', function() {
+      it('should return the first vector divded by the last', () => {
         var x = Math.round((a.x / b.x) * 10000);
         var y = Math.round((a.y / b.y) * 10000);
 
@@ -116,42 +112,42 @@ describe('Vector', function() {
       });
     });
 
-    describe('#len()', function() {
-      it('should return the length of the vector', function() {
+    describe('#len()', () => {
+      it('should return the length of the vector', () => {
         assert.equal(6, Math.floor(a.len()));
         assert.equal(10, Math.floor(b.len()));
       });
     });
 
-    describe('#lenSq()', function() {
-      it('should return the length of the vector squared', function() {
+    describe('#lenSq()', () => {
+      it('should return the length of the vector squared', () => {
         assert.equal(37, Math.floor(a.lenSq()));
         assert.equal(116, Math.floor(b.lenSq()));
       });
     });
 
-    describe('#normalize()', function() {
+    describe('#normalize()', () => {
       var result = a.normalize();
 
-      it('should return the normal of the vector', function() {
+      it('should return the normal of the vector', () => {
         assert.equal(986, Math.round(result.x * 1000));
         assert.equal(164, Math.round(result.y * 1000));
       });
     });
 
-    describe('#angle()', function() {
-      it('should return the angle between the two vectors', function() {
+    describe('#angle()', () => {
+      it('should return the angle between the two vectors', () => {
         assert.equal(12339, Math.round(a.angle(b) * 1000));
       });
 
-      it('should return the exact same as #angleDeg()', function() {
+      it('should return the exact same as #angleDeg()', () => {
         assert.equal(a.angleDeg(b), a.angle(b));
       });
 
-      it('should return the same from #angle() and' +
-        ' #angleRad() but in rad and deg', function() {
-        assert.equal(a.angle(b), a.angleRad(b) * 180 / Math.PI);
-      });
+      it(`should return the same from #angle() and
+          #angleRad() but in rad and deg`, () => {
+            assert.equal(a.angle(b), a.angleRad(b) * 180 / Math.PI);
+          });
     });
   });
 });
